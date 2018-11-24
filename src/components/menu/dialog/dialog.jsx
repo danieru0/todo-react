404619a3c4627daf3e3ecf4720c6e6538833b200
Daniel Dąbrowski <elosik123@onet.pl>
2018-11-24 22:24:20 +0100

    User settings, delete account dialog

---
+++ src/components/menu/dialog/dialog.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import './dialog.css';
+
+class DialogComponent extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+        open: true
+    }
+  }
+
+  componentDidMount() {
+      //this.setState({ open: true });
+  }
+
+  handleClose = () => {
+      this.setState({ open: false });
+  }
+
+  render() {
+    return (
+        <Dialog
+            open={this.state.open}
+            onClose={this.handleClose}
+        >
+            <DialogTitle id="alert-dialog-title">{'Are you sure about that?'}</DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    Deleting your account means removing all your tasks.
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={(e) => { this.props.onClick('no'); this.handleClose(); }}>No</Button>
+                <Button onClick={(e) => { this.props.onClick('yes'); this.handleClose(); }}>Yes!</Button>
+            </DialogActions>
+        </Dialog>
+    );
+  }
+}
+
+export default DialogComponent;
--- src/components/todo/user/user.jsx
+++ src/components/todo/user/user.jsx
@@ -1,14 +1,103 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '../../menu/dialog/dialog';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { withStyles } from '@material-ui/core/styles';
 import './user.css';
 
+const styles = {
+  save: {
+    backgroundColor: '#4a148c',
+    color: '#ffffff',
+    '&:hover': {
+      backgroundColor: '#7E57C2'
+    }
+  },
+  delete: {
+    backgroundColor: '#e00032',
+    color: '#ffffff',
+    '&:hover': {
+      backgroundColor: '#f36c60'
+    }
+  },
+  loader: {
+    color: '#4a148c'
+  }
+}
+
 class User extends Component {
+  constructor() {
+    super();
+    this.state = {
+      submitted: false,
+      deleteDialog: false
+    }
+  }
+
+  handleUserSubmit = e => {
+    e.preventDefault();
+    this.setState({ submitted: true });
+  }
+
+  handleDeleteButton = () => {
+    this.setState({ deleteDialog: true, submitted: true });
+  }
+
+  handleDeleteDialog = (type) => {
+    switch(type) {
+      case 'no':
+        this.setState({ submitted: false, deleteDialog: false });
+        break;
+      case 'yes':
+        this.setState({ submitted: false, deleteDialog: false });
+        alert('deleting account...');
+        break;
+      default: 
+        break;
+    }
+  }
+
   render() {
+    const { classes } = this.props;
     return (
       <div className="user">
-        UserEWFEFWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+        <form onSubmit={this.handleUserSubmit} className="user__form">
+          <p className="user__settings-title">Avatar<span className="title-small"> 64x64</span></p>
+          <div className="settings-group">
+            <img className="settings__avatar" alt="" src="https://react-materialize.github.io/img/yuna.jpg"></img>
+            <input className="settings__file" type="file"></input>
+          </div>
+          <p className="user__settings-title">Background<span className="title-small"> 250x180</span></p>
+          <div className="settings-group">
+            <img className="settings__background" alt="" src="https://react-materialize.github.io/img/office.jpg"></img>
+            <input className="settings__file" type="file"></input>
+          </div>
+          <p className="user__settings-title">Password</p>
+          <div className="settings-group">
+            <input name="old-password" type="password" placeholder="Your old password" className="user__password"></input>
+            <input name="new-password" type="password" placeholder="Your new password" className="user__password"></input>
+          </div>
+          <div className="settings-group">
+            <Button
+              disabled={this.state.submitted}
+              type="submit"
+              variant="contained"
+              className={classes.save}
+            > { this.state.submitted ? <CircularProgress className={classes.loader} size={20} /> : 'Save' } </Button>
+            <Button
+              disabled={this.state.submitted}
+              variant="contained"
+              className={classes.delete}
+              onClick={this.handleDeleteButton}
+            > { this.state.submitted ? <CircularProgress className={classes.loader} size={20} /> : 'Delete account' } </Button>
+          </div>
+        </form>
+        {
+          this.state.deleteDialog ? <Dialog onClick={this.handleDeleteDialog} /> : '' 
+        }
       </div>
     );
   }
 }
 
-export default User;
+export default withStyles(styles)(User);
